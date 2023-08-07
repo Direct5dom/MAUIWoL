b93c86a711161453c8b201d4d82ca7d947a7ec1c
Direct5dom <sixiaolong2018@outlook.com>
2023-08-08 04:44:59 +0800

    修改Windows下的默认窗口大小。

--- MAUIWoL/App.xaml.cs
+++ MAUIWoL/App.xaml.cs
@@ -1,11 +1,33 @@
-﻿namespace MAUIWoL;
+﻿#if WINDOWS
+using Microsoft.UI;
+using Microsoft.UI.Windowing;
+using Windows.Graphics;
+#endif
+
+namespace MAUIWoL;
 
 public partial class App : Application
 {
-	public App()
-	{
-		InitializeComponent();
+    const int WindowWidth = 450;
+    const int WindowHeight = 800;
+    public App()
+    {
+        InitializeComponent();
 
-		MainPage = new AppShell();
-	}
+        MainPage = new AppShell();
+
+        Microsoft.Maui.Handlers.WindowHandler.Mapper.AppendToMapping(nameof(IWindow), (handler, view) =>
+        {
+            //设置界面宽高
+#if WINDOWS
+            var mauiWindow = handler.VirtualView;
+            var nativeWindow = handler.PlatformView;
+            nativeWindow.Activate();
+            IntPtr windowHandle = WinRT.Interop.WindowNative.GetWindowHandle(nativeWindow);
+            WindowId windowId = Microsoft.UI.Win32Interop.GetWindowIdFromWindow(windowHandle);
+            AppWindow appWindow = Microsoft.UI.Windowing.AppWindow.GetFromWindowId(windowId);
+            appWindow.Resize(new SizeInt32(WindowWidth, WindowHeight));
+#endif
+        });
+    }
 }
